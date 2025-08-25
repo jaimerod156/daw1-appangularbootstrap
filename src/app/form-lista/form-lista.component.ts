723eab36fa7847d9cf1521c58356e429b43b3b59
jaimerod156 <jaimerodriguez156@gmail.com>
2025-08-24 21:03:46 -0400

    form-lista uso de directivas for, if, else if , else

--- src/app/form-lista/form-lista.component.ts
+++ src/app/form-lista/form-lista.component.ts
@@ -9,4 +9,14 @@ import { Component } from '@angular/core';
 })
 export class FormListaComponent {
 
+  alumnos = [
+    {nombre: "Joan", apellido: "Osorio", nota: 18},
+    {nombre: "Jose", apellido: "Lagos", nota: 5},
+    {nombre: "Juan", apellido: "Mazuelos", nota: 10},
+    {nombre: "Luis", apellido: "Ayala", nota: 12},
+    {nombre: "Luis", apellido: "Rojas", nota: 20},
+    {nombre: "Nelson", apellido: "Laura", nota: 17},
+    {nombre: "Pedro", apellido: "Mejia", nota: 6},
+    {nombre: "Abel", apellido: "Perez", nota: 11}
+  ]
 }
