7d65ccae08aca62b67e28b7717ecae0feeefb246
jaimerod156 <jaimerodriguez156@gmail.com>
2025-08-25 20:04:41 -0400

    ejemplo uso pipes

--- src/app/form-pipes/form-pipes.component.ts
+++ src/app/form-pipes/form-pipes.component.ts
@@ -8,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrl: './form-pipes.component.scss'
 })
 export class FormPipesComponent {
-
+  
+  currentDate = new Date();
+  
+  clientes = [
+    {nombre: 'Karla Gomez'},
+    {nombre: 'luis salvatierra'},
+    {nombre: 'CAMILA HURTADO'},
+    {nombre: 'Pedro navaja'}, 
+    {nombre: 'rodrigo Human'}
+  ]
+  cursos = [
+    {nombre: 'Angular ', precio: 2000},
+    {nombre: 'Base de Datos', precio: 3500},
+    {nombre: 'Gestion de Proyectos', precio: 4500},
+    {nombre: 'AWS - Developer', precio: 5000},
+  ]
 }
